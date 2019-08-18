bc475389e48afc3ffece7308da91fb8506a3b92e
Nedson202 <simeonsamson4@gmail..com>
2019-08-18 02:10:25 +0100

    dev-remove-user-analytics
    * Remove unused user analytics
    * Update profile style for mobile

--- src/Components/UserProfile/UserDetails.jsx
+++ src/Components/UserProfile/UserDetails.jsx
@@ -1,48 +1,35 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './_UserProfile.scss';
 
 const UserDetails = (props) => {
-  const { toggleEditForm, user } = props;
+  const { user } = props;
   return (
-    <Fragment>
-      <div className="profile-details">
-        <div className="profile-details__image">
-          <img
-            src={user.picture}
-            id="show-image"
-            height="200px"
-            width="200px"
-            alt="Avatar"
-          />
-        </div>
-        <div className="profile-details__info">
-          <span>Username</span>
-          <h4>{user.username}</h4>
-          <span>Email</span>
-          <h4>{user.email}</h4>
-          <hr />
-          <button
-            type="button"
-            className="btn btn-raised"
-            id="edit-button"
-            onClick={toggleEditForm}
-          >
-            Edit profile
-          </button>
-        </div>
+    <div className="profile-details">
+      <div className="profile-details__image">
+        <img
+          src={user.picture}
+          id="show-image"
+          height="200px"
+          width="200px"
+          alt="Avatar"
+        />
       </div>
-    </Fragment>
+      <div className="profile-details__info">
+        <span>Username</span>
+        <h4>{user.username}</h4>
+        <span>Email</span>
+        <h4>{user.email}</h4>
+      </div>
+    </div>
   );
 };
 
 UserDetails.propTypes = {
-  toggleEditForm: PropTypes.func,
   user: PropTypes.object,
 };
 
 UserDetails.defaultProps = {
-  toggleEditForm: () => { },
   user: {},
 };
 
